dc0bf4a677e7e95e989f4731158ddff28f05d29e
Wesley Scholl <clients@konjoweb.com>
2019-04-08 11:40:16 -0400

    planning and some basic scaffolding

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  jwtSecret: "K0nJ0b3aUT1fUlP4sS10n",
+  jwtSession: {
+    session: false
+  }
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const parser = require("body-parser");
+const methodOverride = require("method-override");
+// const passport = require("./config/passport")();
+// const userController = require("./controllers/users.js");
+// const songController = require("./controllers/song");
+// const voteController = require("./controllers/votes");
+
+const app = express();
+
+app.use(cors());
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+app.use(methodOverride("_method"));
+// app.use(passport.initialize());
+
+app.get("/", function(req, res) {
+  res.json("Konjo!");
+});
+
+// app.use("/songs", songController);
+// app.use("/users", userController);
+// app.use("/votes", voteController);
+
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log(`Running on PORT: ${app.get("port")}`);
+});
