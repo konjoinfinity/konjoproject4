88640588a8f6d13fd10c9e46563cd98d8a1667ea
Wesley Scholl <clients@konjoweb.com>
2019-05-08 05:27:42 -0400

    added confirm password field to user signup method

--- controllers/user.js
+++ controllers/user.js
@@ -8,33 +8,39 @@ const router = express.Router();
 
 router.post("/signup", (req, res) => {
   if (req.body.email && req.body.password) {
-    let newUser = {
-      email: req.body.email,
-      password: req.body.password
-    };
-    User.findOne({ email: req.body.email }).then(user => {
-      if (!user) {
-        User.create(newUser).then(user => {
-          if (user) {
-            var payload = {
-              id: newUser.id
-            };
-            var token = jwt.encode(payload, config.jwtSecret);
-            res.json({
-              token: token
-            });
-          } else {
-            res.json({
-              error: "Error in user creation"
-            });
-          }
-        });
-      } else {
-        res.json({
-          error: "Email has already been registered"
-        });
-      }
-    });
+    if (req.body.password === req.body.confirmpass) {
+      let newUser = {
+        email: req.body.email,
+        password: req.body.password
+      };
+      User.findOne({ email: req.body.email }).then(user => {
+        if (!user) {
+          User.create(newUser).then(user => {
+            if (user) {
+              var payload = {
+                id: newUser.id
+              };
+              var token = jwt.encode(payload, config.jwtSecret);
+              res.json({
+                token: token
+              });
+            } else {
+              res.json({
+                error: "Error in user creation"
+              });
+            }
+          });
+        } else {
+          res.json({
+            error: "Email has already been registered"
+          });
+        }
+      });
+    } else {
+      res.json({
+        error: "Passwords don't match"
+      });
+    }
   } else {
     res.json({
       error: "Please enter both email and password"
