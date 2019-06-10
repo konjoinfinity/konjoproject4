b81ca691f0c8ea27c6c186737cfe248f25eb70f1
Wesley Scholl <clients@konjoweb.com>
2019-06-10 11:52:30 -0400

    added jwt token auth for change password method

--- controllers/user.js
+++ controllers/user.js
@@ -82,42 +82,64 @@ router.post("/login", (req, res) => {
 });
 
 router.post("/changepass", (req, res) => {
-  if (req.body.email && req.body.password) {
-    if (req.body.newpassword === req.body.confirmnewpassword) {
-      User.findOne({ email: req.body.email }).then(user => {
-        if (user) {
-          let success = user.comparePassword(req.body.password, user.password);
-          if (success === true) {
-            let newpassword = bcrypt.hashSync(req.body.newpassword, saltRounds);
-            User.findOneAndUpdate(
-              { email: req.body.email },
-              { $set: { password: newpassword } },
-              { new: true }
-            )
-              .then(user => {
-                res.json("Password has been changed");
-              })
-              .catch(err => console.log(err));
-          } else {
-            res.json({
-              error: "Password does not match email account"
-            });
-          }
+  var token = req.headers["user-token"];
+  if (!token)
+    return res.status(401).send({ auth: false, message: "No token provided." });
+
+  try {
+    var decoded = jwt.decode(token, config.jwtSecret);
+    User.findById(decoded.id, { password: 0 }, function(err, user) {
+      if (err)
+        return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+      if (req.body.email && req.body.password) {
+        if (req.body.newpassword === req.body.confirmnewpassword) {
+          User.findOne({ email: req.body.email }).then(user => {
+            if (user) {
+              let success = user.comparePassword(
+                req.body.password,
+                user.password
+              );
+              if (success === true) {
+                let newpassword = bcrypt.hashSync(
+                  req.body.newpassword,
+                  saltRounds
+                );
+                User.findOneAndUpdate(
+                  { email: req.body.email },
+                  { $set: { password: newpassword } },
+                  { new: true }
+                )
+                  .then(user => {
+                    res.json("Password has been changed");
+                  })
+                  .catch(err => console.log(err));
+              } else {
+                res.json({
+                  error: "Password does not match email account"
+                });
+              }
+            } else {
+              res.json({
+                error: "Email has not been registered"
+              });
+            }
+          });
         } else {
           res.json({
-            error: "Email has not been registered"
+            error: "New passwords do not match"
           });
         }
-      });
-    } else {
-      res.json({
-        error: "New passwords do not match"
-      });
-    }
-  } else {
-    res.json({
-      error: "Please enter both email and password"
+      } else {
+        res.json({
+          error: "Please enter both email and password"
+        });
+      }
     });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ auth: false, message: "Failed to authenticate token." });
   }
 });
 
