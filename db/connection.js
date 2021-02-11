6f0eddf1fb68e0349a0394ddc43dd595f2a24f24
Wesley Scholl <clients@konjoweb.com>
2021-02-11 11:32:34 -0500

    updated mongoose connection to include new unifiedTopology option to prevent deprecation

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   console.log("Production Database Connection Successful");
 } else {
-  mongoose.connect("mongodb://localhost/project4", { useNewUrlParser: true });
+  mongoose.connect("mongodb://localhost/project4", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   console.log("Development Database Connection Successful");
 }
 
