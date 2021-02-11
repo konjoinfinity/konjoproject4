10e91177b3dded1b37c3f6e15a43fddfd340cc66
Wesley Scholl <clients@konjoweb.com>
2021-02-11 16:44:47 -0500

    added useFindAndModify & useCreateIndex options for mongoose to prevent warnings

--- db/connection.js
+++ db/connection.js
@@ -4,13 +4,17 @@ mongoose.Promise = Promise;
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
   });
   console.log("Production Database Connection Successful");
 } else {
   mongoose.connect("mongodb://localhost/project4", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
   });
   console.log("Development Database Connection Successful");
 }
