2b060f4111f42c4ae7c768ea3f4055517e293338
Wesley Scholl <clients@konjoweb.com>
2022-11-24 18:14:46 -0500

    updated now functional

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,28 @@ mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+useUnifiedTopology: true,
+serverSelectionTimeoutMS: 5000,
+autoIndex: false, // Don't build indexes
+maxPoolSize: 10, // Maintain up to 10 socket connections
+serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+family: 4 
   }, function(err) {
     if (err) {throw err;
     } else {console.log("Production Database Connection Successful");}
     })
 } else {
-  mongoose.connect("mongodb://localhost/project4"), { 
+  mongoose.connect("mongodb://127.0.0.1/project4"), { 
+    useNewUrlParser: true,
+useUnifiedTopology: true,
+serverSelectionTimeoutMS: 5000,
+autoIndex: false, // Don't build indexes
+maxPoolSize: 10, // Maintain up to 10 socket connections
+serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+family: 4 
   }, function(err) {
     if (err) {throw err;
     } else {console.log("Development Database Connection Successful")}
