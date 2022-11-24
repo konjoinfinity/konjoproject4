13f34aa7e0211cc456d57a01e82248d6789bab40
Wesley Scholl <clients@konjoweb.com>
2022-11-24 10:45:27 -0500

    updated mongoose to v6 and updated connection

--- db/connection.js
+++ db/connection.js
@@ -3,20 +3,16 @@ mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  });
-  console.log("Production Database Connection Successful");
+  }, function(err) {
+    if (err) {throw err;
+    } else {console.log("Production Database Connection Successful");}
+    })
 } else {
-  mongoose.connect("mongodb://localhost/project4", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  });
-  console.log("Development Database Connection Successful");
+  mongoose.connect("mongodb://localhost/project4"), { 
+  }, function(err) {
+    if (err) {throw err;
+    } else {console.log("Development Database Connection Successful")}
+    };
 }
 
 module.exports = mongoose;
